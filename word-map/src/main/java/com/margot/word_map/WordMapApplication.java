99badaab0a56007ffad73950d272d816da2949d3
Alex Kolosov <megokolos@gmail.com>
2025-03-25 19:09:22 +0300

    create starter project

---
+++ word-map/src/main/java/com/margot/word_map/WordMapApplication.java
@@ -0,0 +1,13 @@
+package com.margot.word_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordMapApplication.class, args);
+	}
+
+}
---
+++ word-map/src/test/java/com/margot/word_map/WordMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margot.word_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
