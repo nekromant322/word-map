47b612cc3be72b70f5393fbce6da715b1e9bce01
Alex Kolosov <alex.kolosov.work@gmail.com>
2025-03-31 22:29:52 +0300

    checkstyle fix

--- word-map/src/main/java/com/margot/word_map/service/users_service/UsersServiceImpl.java
+++ word-map/src/main/java/com/margot/word_map/service/users_service/UsersServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
