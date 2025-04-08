450ec461a8ae8eaffa721b761c56baf88776d4f8
bahertylop <levichankin@gmail.com>
2025-04-08 13:11:10 +0300

    VM-17: аннотации swagger для dto используемых в AuthController

--- word-map/src/main/java/com/margot/word_map/dto/response/DictionaryWordResponse.java
+++ word-map/src/main/java/com/margot/word_map/dto/response/DictionaryWordResponse.java
@@ -1,5 +1,6 @@
 package com.margot.word_map.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +10,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "Ответ с информацией о слове из словаря")
 public class DictionaryWordResponse {
 
+    @Schema(description = "Идентификатор слова в словаре", example = "123")
     private Long id;
 
+    @Schema(description = "Слово из словаря", example = "лопата")
     private String word;
 
+    @Schema(description = "Описание слова", example = "Инструмент для копания земли")
     private String description;
 }
--- word-map/src/main/java/com/margot/word_map/dto/response/TokenResponse.java
+++ word-map/src/main/java/com/margot/word_map/dto/response/TokenResponse.java
@@ -1,11 +1,17 @@
 package com.margot.word_map.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Ответ, содержащий access и refresh токены")
 public class TokenResponse {
+
+    @Schema(description = "JWT access токен, используемый для авторизации", example = "eyJhbGciOiJIUzI1...")
     private String accessToken;
+
+    @Schema(description = "JWT refresh токен, используемый для обновления access токена", example = "dGhpcy1pc...")
     private String refreshToken;
 }
