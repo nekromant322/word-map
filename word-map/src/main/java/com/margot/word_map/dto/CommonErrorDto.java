46c54e26468b78429c2f1b9501638e151c5dd9c1
bahertylop <levichankin@gmail.com>
2025-04-12 01:10:27 +0300

    VM-21: документироваие CommonErrorDto

--- word-map/src/main/java/com/margot/word_map/dto/CommonErrorDto.java
+++ word-map/src/main/java/com/margot/word_map/dto/CommonErrorDto.java
@@ -1,5 +1,6 @@
 package com.margot.word_map.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +12,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "Общая структура ошибки, возвращаемая при возникновении исключений")
 public class CommonErrorDto {
 
+    @Schema(description = "Код ошибки", example = "404", required = true)
     private Integer code;
 
+    @Schema(description = "Сообщение об ошибке", example = "Пользователь не найден", required = true)
     private String message;
 
+    @Schema(description = "Дата и время возникновения ошибки", example = "2025-04-12T15:30:00", required = true)
     private LocalDateTime date;
 }
