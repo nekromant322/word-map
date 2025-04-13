29d674ee78e91569ff94c95a79bbc06d19c7c922
bahertylop <levichankin@gmail.com>
2025-04-13 13:14:48 +0300

    VM-13: набросок разбиения эндпоинтов по ролям и правам

--- word-map/src/main/java/com/margot/word_map/config/SecurityConfig.java
+++ word-map/src/main/java/com/margot/word_map/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.margot.word_map.config;
 
 import com.margot.word_map.config.jwt.JwtFilter;
+import com.margot.word_map.model.Admin;
+import com.margot.word_map.model.Rule;
 import com.margot.word_map.service.auth.AdminDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +40,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/auth/admins").authenticated()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
@@ -69,4 +78,47 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
+
+//    private AuthorizationDecision customAccessCheck(Authentication authentication, HttpServletRequest request) {
+//        Admin admin = (Admin) authentication.getPrincipal();
+//
+//        if (admin.getRole() == Admin.ROLE.ADMIN) {
+//            return new AuthorizationDecision(true);
+//        }
+//
+//        String path = request.getRequestURI();
+//        Optional<Rule.RULE> requiredRule = getRuleByPath(path); // ты сам настраиваешь соответствие
+//
+//        return new AuthorizationDecision(
+//                requiredRule.map(rule -> admin.getRules().stream()
+//                                .map(Rule::getName)
+//                                .anyMatch(r -> r == rule))
+//                        .orElse(false)
+//        );
+//    }
+
+//    private Optional<Rule.RULE> getRuleByPath(String path) {
+//        if (path.startsWith("/dictionary/wipe")) {
+//            return Optional.of(Rule.RULE.WIPE_DICTIONARY);
+//        }
+//        if (path.startsWith("/dictionary")) {
+//            return Optional.of(Rule.RULE.MANAGE_DICTIONARY);
+//        }
+//        if (path.startsWith("/rating")) {
+//            return Optional.of(Rule.RULE.MANAGE_RATING);
+//        }
+//        if (path.startsWith("/world")) {
+//            return Optional.of(Rule.RULE.MANAGE_WORLD);
+//        }
+//        if (path.startsWith("/auth/admins") || path.startsWith("/roles")) {
+//            return Optional.of(Rule.RULE.MANAGE_ROLE);
+//        }
+//        if (path.startsWith("/ivent")) {
+//            return Optional.of(Rule.RULE.MANAGE_IVENT);
+//        }
+//        if (path.startsWith("/shop")) {
+//            return Optional.of(Rule.RULE.MANAGE_SHOP);
+//        }
+//        return Optional.empty();
+//    }
 }
