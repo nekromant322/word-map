2d6fed764e842417c61a107160fd0ad8012dc80b
Alex Kolosov <alex.kolosov.work@gmail.com>
2025-04-20 19:35:26 +0300

    rename classes to single object

--- word-map/src/main/java/com/margot/word_map/repository/WordOfferRepository.java
+++ word-map/src/main/java/com/margot/word_map/repository/WordOfferRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface WordsOfferRepository extends JpaRepository<WordOffer, Long> {
+public interface WordOfferRepository extends JpaRepository<WordOffer, Long> {
 
     Optional<WordOffer> findByWord(String word);
 
