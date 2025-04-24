2794686be7778246a9df5e9e5c086cb518d7d40b
bahertylop <levichankin@gmail.com>
2025-04-25 02:03:31 +0300

    frontend: готов к загрузке на сервер, фронтенд запущен в nginx, nginx запущен в докере

--- word-map/src/main/java/com/margot/word_map/config/CorsConfig.java
+++ word-map/src/main/java/com/margot/word_map/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost"));
+        config.setAllowedOrigins(List.of("http://45.8.230.91"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
