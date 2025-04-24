72b150a0825344f30fbdbfd06dfc6a7a5444527b
bahertylop <levichankin@gmail.com>
2025-04-24 11:22:15 +0300

    frontend: ретрай на авторизованные запросы в admins-api.js

--- frontend/js/api/admins-api.js
+++ frontend/js/api/admins-api.js
@@ -1,41 +1,23 @@
 import {getAccessToken} from "../util/jwt.js";
 import {CONFIG} from "../config/api-config.js";
 import {adminsOnPage} from "../config/constants.js";
+import {authorizedFetch} from "./util.js";
 
 export async function getRules() {
-    const token = getAccessToken();
-    const response = await fetch(`${CONFIG.API_BASE_URL}/admins/rules`, {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + token
-        }
+    return authorizedFetch(`${CONFIG.API_BASE_URL}/admins/rules`, {
+        method: "GET"
     });
-
-    return response;
 }
 
 export async function getAdmins(pageNumber) {
     const numbersOnPage = adminsOnPage;
-    const token = getAccessToken();
-
-    const response = await fetch(`${CONFIG.API_BASE_URL}/admins?page=${pageNumber}&size=${numbersOnPage}`, {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + token
-        }
+    return authorizedFetch(`${CONFIG.API_BASE_URL}/admins?page=${pageNumber}&size=${numbersOnPage}`, {
+        method: "GET"
     });
-
-    return response;
 }
 
 export async function getAdmin(adminId) {
-    const token = getAccessToken();
-
-    const response = await fetch(`${CONFIG.API_BASE_URL}/admins/${adminId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + token
-        }
+    return authorizedFetch(`${CONFIG.API_BASE_URL}/admins/${adminId}`, {
+        method: "GET"
     });
-    return response;
 }
