ef34b46855cc99d09e24b08bf7a9b8a39d2ce166
bahertylop <levichankin@gmail.com>
2025-04-27 17:46:51 +0300

    WM-36: указание аннотации над исключением AdminAlreadyExists

--- word-map/src/main/java/com/margot/word_map/exception/AdminAlreadyExistsException.java
+++ word-map/src/main/java/com/margot/word_map/exception/AdminAlreadyExistsException.java
@@ -1,6 +1,13 @@
 package com.margot.word_map.exception;
 
-public class AdminAlreadyExistsException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AdminAlreadyExistsException extends BaseException {
+
+    public AdminAlreadyExistsException() {
+    }
 
     public AdminAlreadyExistsException(String message) {
         super(message);
