4f49011c0478959196375769591266982709fc2a
Alex Kolosov <alex.kolosov.work@gmail.com>
2025-05-01 23:03:17 +0300

    add method for searching language by id

--- word-map/src/main/java/com/margot/word_map/service/language/LanguageService.java
+++ word-map/src/main/java/com/margot/word_map/service/language/LanguageService.java
@@ -3,12 +3,14 @@ package com.margot.word_map.service.language;
 import com.margot.word_map.dto.LanguageDto;
 import com.margot.word_map.exception.LanguageNotFoundException;
 import com.margot.word_map.mapper.LanguageMapper;
+import com.margot.word_map.model.Language;
 import com.margot.word_map.repository.LanguageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -35,6 +37,10 @@ public class LanguageService {
             log.info("language with id {} not found", id);
             return new LanguageNotFoundException("language with id " + id + " not found");
         }));
+    }
+
+    public Optional<Language> findById(Long id) {
+        return languageRepository.findById(id);
     }
 
     public void createLanguage() {
