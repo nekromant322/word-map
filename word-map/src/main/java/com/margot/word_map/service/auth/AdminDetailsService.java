73256d8f3e9cd226289d2fd5c147239b2d24257c
retrolad <retrol4d@gmail.com>
2026-01-11 21:59:15 +0400

    WM-218: remove User related services

--- word-map/src/main/java/com/margot/word_map/service/auth/AdminDetailsService.java
+++ word-map/src/main/java/com/margot/word_map/service/auth/AdminDetailsService.java
@@ -1,7 +1,6 @@
 package com.margot.word_map.service.auth;
 
 import com.margot.word_map.repository.AdminRepository;
-import com.margot.word_map.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,13 +12,11 @@ import org.springframework.stereotype.Service;
 public class AdminDetailsService implements UserDetailsService {
 
     private final AdminRepository adminRepository;
-    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return adminRepository.findByEmail(email)
                 .map(admin -> (UserDetails) admin)
-                .or(() -> userRepository.findByEmail(email).map(user -> (UserDetails) user))
                 .orElseThrow(() -> new UsernameNotFoundException("user not found" + email));
     }
 }
