5277387f48c635b50affd2e551e3636f179f8e55
retrolad <retrol4d@gmail.com>
2026-01-29 22:58:29 +0400

    WM-155: refactor GET /rule/option

--- word-map/src/main/java/com/margot/word_map/mapper/RuleMapper.java
+++ word-map/src/main/java/com/margot/word_map/mapper/RuleMapper.java
@@ -1,6 +1,7 @@
 package com.margot.word_map.mapper;
 
 import com.margot.word_map.dto.RuleDto;
+import com.margot.word_map.dto.OptionDto;
 import com.margot.word_map.model.Rule;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,14 @@ public class RuleMapper {
         return RuleDto.builder()
                 .ruleID(rule.getId())
                 .ruleName(rule.getName().name())
+                .build();
+    }
+
+    public OptionDto toOptionDto(Rule rule) {
+        return OptionDto.builder()
+                .label(rule.getName().name())
+                .value(rule.getId().toString())
+                .description("")
                 .build();
     }
 }
