e6851998a551fd9c62cdab8608d3189d75f4cebd
retrolad <retrol4d@gmail.com>
2026-01-31 03:53:38 +0400

    WM-228: implement thread local admin cache

---
+++ word-map/src/main/java/com/margot/word_map/config/filter/AdminCacheCleanupFilter.java
@@ -0,0 +1,31 @@
+package com.margot.word_map.config.filter;
+
+import com.margot.word_map.utils.security.ThreadLocalAdminCache;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AdminCacheCleanupFilter extends OncePerRequestFilter {
+
+    private final ThreadLocalAdminCache adminCache;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            adminCache.clear();
+        }
+    }
+}
