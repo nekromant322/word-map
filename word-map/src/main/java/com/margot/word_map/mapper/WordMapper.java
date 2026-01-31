6bc0770aad65f4e63b83c1b6ebe78f886cc13cb4
Inger2 <oleg.sidor2121@gmail.com>
2026-01-31 23:33:12 +0200

    Изменение метода поиска слова в словаре согласно тз https://override-platform.atlassian.net/browse/WM-166?actionerId=70121%3A6e4a1af2-0eea-4745-be25-5b971d7ec0f3&sourceType=assign

--- word-map/src/main/java/com/margot/word_map/mapper/WordMapper.java
+++ word-map/src/main/java/com/margot/word_map/mapper/WordMapper.java
@@ -1,20 +1,11 @@
 package com.margot.word_map.mapper;
 
 import com.margot.word_map.dto.response.DictionaryDetailedWordResponse;
-import com.margot.word_map.dto.response.DictionaryWordResponse;
 import com.margot.word_map.model.Word;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WordMapper {
-
-    public DictionaryWordResponse toDictionaryWordResponse(Word word) {
-        return DictionaryWordResponse.builder()
-                .id(word.getId())
-                .word(word.getWord())
-                .description(word.getDescription())
-                .build();
-    }
 
     public DictionaryDetailedWordResponse toDictionaryDetailedWordResponse(Word word) {
         return DictionaryDetailedWordResponse.builder()
