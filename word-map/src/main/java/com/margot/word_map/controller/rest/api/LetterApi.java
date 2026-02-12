9db355a759b924b1d783785e2e30569fae9d7fa7
retrolad <retrol4d@gmail.com>
2026-02-12 21:30:30 +0400

    feat: WM-191 implement PUT/letter/{id}

--- word-map/src/main/java/com/margot/word_map/controller/rest/api/LetterApi.java
+++ word-map/src/main/java/com/margot/word_map/controller/rest/api/LetterApi.java
@@ -2,6 +2,7 @@ package com.margot.word_map.controller.rest.api;
 
 import com.margot.word_map.dto.LetterDto;
 import com.margot.word_map.dto.request.CreateLetterRequest;
+import com.margot.word_map.dto.request.UpdateLetterRequest;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,9 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "LetterController",
@@ -42,4 +41,28 @@ public interface LetterApi {
     )
     @PostMapping
     LetterDto createLetter(@RequestBody @Valid CreateLetterRequest request);
+
+    @Operation(
+            summary = "Редактирование буквы",
+            description = "Метод редактирования буквы алфавита",
+            externalDocs = @ExternalDocumentation(
+                    description = "документация запроса в Confluence",
+                    url = "https://override-platform.atlassian.net/wiki/spaces/W/pages/404553729/PUT+letter+id"
+            )
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Буква отредактирована"),
+                    @ApiResponse(responseCode = "400", description = "Некорректный формат ввода"),
+                    @ApiResponse(responseCode = "401", description = "Токен доступа недействителен"),
+                    @ApiResponse(responseCode = "403", description = "Аккаунт заблокирован"),
+                    @ApiResponse(responseCode = "403", description = "Недостаточно прав"),
+                    @ApiResponse(responseCode = "404", description = "Аккаунт не найден"),
+                    @ApiResponse(responseCode = "409", description = "Буква не найдена")
+            }
+    )
+    @PutMapping("/{id}")
+    LetterDto updateLetter(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateLetterRequest request);
 }
--- word-map/src/main/java/com/margot/word_map/repository/map/LetterRepository.java
+++ word-map/src/main/java/com/margot/word_map/repository/map/LetterRepository.java
@@ -4,9 +4,11 @@ import com.margot.word_map.model.Language;
 import com.margot.word_map.model.map.Letter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LetterRepository extends JpaRepository<Letter, Long> {
@@ -20,4 +22,11 @@ public interface LetterRepository extends JpaRepository<Letter, Long> {
                 AND l.language = :language
             """)
     boolean existsByLetterAndLanguage(Character letter, Language language);
+
+    @Query("""
+            SELECT l FROM Letter l
+            JOIN FETCH l.language
+            WHERE l.id = :id
+            """)
+    Optional<Letter> findByIdWithLanguage(@Param("id") Long id);
 }
