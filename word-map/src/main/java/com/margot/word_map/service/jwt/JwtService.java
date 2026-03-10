e7d11c62ea2061f8ac20811d0467d71166bd29f8
Inger2 <oleg.sidor2121@gmail.com>
2026-03-10 05:23:19 +0200

    quick fix

--- word-map/src/main/java/com/margot/word_map/service/jwt/JwtService.java
+++ word-map/src/main/java/com/margot/word_map/service/jwt/JwtService.java
@@ -14,6 +14,7 @@ import javax.crypto.SecretKey;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 import java.util.function.Function;
 
 @Service
@@ -61,7 +62,7 @@ public class JwtService {
                 .email(claims.getSubject())
                 .id(claims.get("id", Long.class))
                 .role(claims.get("role", String.class))
-                .rule(claims.get("rules", String.class))
+                .rules(claims.get("rules", List.class))
                 .build();
     }
 
